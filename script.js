5dfefd8522172e1d8d12c694e2870017ffa7ee04
Katarina Packova <katarina.packova@gmail.com>
2025-03-22 13:19:02 +0100

    Uprava script js file

---
+++ script.js
@@ -0,0 +1,57 @@
+const plat = 40000;
+const naklady = plat * 0.6;
+const danSazba = 0.15;
+const dan = (plat - naklady) * danSazba;
+document.body.innerHTML += Math.floor(dan);
+
+/* document.body.innerHTML += 'Ahoj svete!' + ' ' + 'Ahoj!'; */
+
+/* Délka filmu */
+/* document.body.innerHTML += 'Délka filmu:';
+document.body.innerHTML += ; */
+
+/* Cviceni Vyplata 
+document.body.innerHTML += 'Výplata';
+document.body.innerHTML += ': ';
+document.body.innerHTML += 7 * 320 * 21;
+document.body.innerHTML += ' Kc.';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 'Daň';
+document.body.innerHTML += ': ';
+document.body.innerHTML += Math.floor((47040 - 47040 * 0.6) * 0.15);
+document.body.innerHTML += ' Kc.';
+*/
+
+/* document.body.innerHTML += Math.floor(Math.random() * 6) + 1;
+document.body.innerHTML += '<br>'; */
+
+/* document.body.innerHTML += Math.round(12.333);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.floor(12.333);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.ceil(12.333);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.random();
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.floor(Math.random() * 6) + 1;
+document.body.innerHTML += '<br>'; */
+
+/* 
+document.body.innerHTML += 'Ahoj svete!';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 42;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 42;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 2 + 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 2 - 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 2 * 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 12 / 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 2 + 3 * 4;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 27 % 4;
+ */
